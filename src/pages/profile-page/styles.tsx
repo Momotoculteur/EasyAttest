6485277e8f40913b91abd9a543a0822eeb2b96dd
Bastien MAURICE <bastien_maurice@hotmail.com>
2020-11-03 23:41:43 +0100

    Ajout boutton UI

---
+++ src/pages/profile-page/styles.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    global: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#fff',
+        justifyContent: 'space-around',
+    },
+
+    // PROFIL SECTION
+    viewProfilSection: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+
+        flex: 5
+    },
+    
+    textHeader: {
+        fontSize: 30,
+        flex: 1,
+        fontWeight: 'bold'
+    },
+
+    // BOUTTON SECTION
+    viewButtonSection: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+
+});
