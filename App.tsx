4bcd96477a02684abcb7fa4b5bed0dc31b148857
Bastien MAURICE <bastien_maurice@hotmail.com>
2020-11-04 21:10:56 +0100

    creation des tables

--- App.tsx
+++ App.tsx
@@ -10,21 +10,45 @@ export default function App() {
 
     db.transaction(tx => {
         tx.executeSql(
-            "create table if not exists user (user_id integer primary key not null, prenom text, nom text, date_naissance text, lieu_naissance text, adresse text, ville text, code_postal text);"
+            "create table if not exists\
+                user (\
+                    user_id integer primary key autoincrement not null,\
+                    prenom text not null,\
+                    nom text not null,\
+                    date_naissance text not null,\
+                    lieu_naissance text not null,\
+                    adresse text not null,\
+                    ville text not null,\
+                    code_postal text not null\
+            );"
         );
 
         tx.executeSql(
-            "create table if not exists motif (motif_id integer primary key not null, intitule text, description text)"
+            "create table if not exists\
+                motif (\
+                    motif_id integer primary key autoincrement not null,\
+                    intitule text not null,\
+                    description text not null)"
         );
 
         tx.executeSql(
-            "create table if not exists attestation (attestation_id integer primary key not null, date_sortie text, heure_sortie text, user_id int, motif_id int, foreign key motif_id references motif(motif_id), foreign key user_id references user(user_id));"
+            "create table if not exists\
+                attestation (\
+                    attestation_id integer primary key autoincrement not null,\
+                    date_sortie text not null,\
+                    heure_sortie text not null,\
+                    user_id int not null,\
+                    motif_id int not null,\
+                    foreign key (motif_id) references motif(motif_id),\
+                    foreign key (user_id) references user(user_id)\
+            );"
         );
-
+                
+        /*
         tx.executeSql("insert into motif (intitule, description) values ('coucou', 'slt bg cm tu vq')");
         tx.executeSql("select * from motif", [], (_, { rows }) =>
             console.log(JSON.stringify(rows))
-        );
+        );*/
     }, (e) => { console.log("ERREUR + " + e) },
         () => { console.log("OK + ") }
     );
--- src/components/atoms/momotoculteur-text-input/momotoculteurCheckbox.tsx
+++ src/components/atoms/momotoculteur-text-input/momotoculteurCheckbox.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { View, Text } from 'react-native';
 import { Checkbox } from 'react-native-paper';
 import { color } from 'react-native-reanimated';
 import { ECheckboxState } from '../../shared/enum/ECheckboxState';
@@ -11,27 +12,37 @@ export default class MomotoculteurCheckbox extends React.Component {
     private currentState: ECheckboxState;
     state = {
         checked: false
-    };  
+    };
 
     constructor(props: any) {
         super(props);
         this.currentState = ECheckboxState.UNCHECKED;
-        
+
     }
 
     render() {
         const { checked } = this.state;
         return (
-            <Checkbox.Android
-                status={checked ? 'checked' : 'unchecked'}
-                onPress={() => {
-                    this.setState({checked: !checked});
-                    console.log(checked)
-                }}
-                color='#e50d54'
-                uncheckedColor='gray'
-                
-            />
+            <View style={{flex: 1, flexDirection: 'row', justifyContent:'space-between'}}>
+                <View>
+                     <Checkbox.Android 
+                    status={checked ? 'checked' : 'unchecked'}
+                    onPress={() => {
+                        this.setState({ checked: !checked });
+                    }}
+                    color='#e50d54'
+                    uncheckedColor='gray'
+                />
+                </View>
+               
+                <Text style={{flex: 1}}
+                    onPress={() => {
+                        this.setState({ checked: !checked });
+                    }}
+                >
+                    Coucou
+                </Text>
+            </View>
         );
     }
 
