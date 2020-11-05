e31c3dc2034ea5d7bf7d252753ac96e60c8561a1
Bastien MAURICE <bastien_maurice@hotmail.com>
2020-11-05 16:16:45 +0100

    bind formulaire recup des infos

--- src/pages/profile-page/create-profil-page/create-profil-page.tsx
+++ src/pages/profile-page/create-profil-page/create-profil-page.tsx
@@ -7,16 +7,75 @@ import { ScrollView, TouchableWithoutFeedback } from "react-native-gesture-handl
 import MomotoculteurCreateProfilForm from "../../../components/molecules/momotoculteur-create-profil-form/momotoculteurCreateProfilForm";
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
-export default function CreateProfilePage({ navigation }) {
-    const [text, setText] = React.useState('');
+
+export default class CreateProfilePage extends React.Component {
+
+
+    private firstName: string;
+    private lastName: string;
+    private birthdate: string;
+    private birthplace: string;
+    private adress: string;
+    private city: string
+    private postalCode: string;
+
+
+    constructor(props: any) {
+        super(props);
+        this.firstName = '';
+        this.lastName = '';
+        this.birthdate = '';
+        this.birthplace = '';
+        this.adress = '';
+        this.city = '';
+        this.postalCode = '';
+    }
+
+    getFirstname(newName: string): void {
+        this.firstName = newName;
+    }
+    getLastname(newLastname: string):void {
+        this.lastName = newLastname;
+    }
+    getBirthdate(newBirthday: string):void {
+        this.birthdate = newBirthday;
+    }
+    getBirthplace(newBirthplace: string): void {
+        this.birthplace = newBirthplace;
+    }
+    getAdress(newAdress: string): void {
+        this.adress = newAdress;
+    }
+    getCity(newCity: string):void {
+        this.city = newCity;
+    }
+    getPostalcode(newPostalcode: string): void {
+        this.postalCode = newPostalcode;
+    }
+
+    createUser() :void {
+
+    }
+
+    
+    render(){
     return (
         <View style={styles.global}>
             <View style={styles.viewFormGlobal}>
-                <KeyboardAwareScrollView style={{flex: 1}}>
+                <KeyboardAwareScrollView style={{ flex: 1 }}>
                     <ScrollView
                         style={{ flexGrow: 1 }}
                         keyboardShouldPersistTaps="handled">
-                        <MomotoculteurCreateProfilForm />
+                        <View style={{ flex: 1, flexDirection: 'column', padding: '5%' }}>
+                            <MomotoculteurTextInput getData={this.getFirstname} label="Prénom" mode="outlined" />
+                            <MomotoculteurTextInput getData={this.getLastname} label="Nom" mode="outlined" />
+                            <MomotoculteurTextInput getData={this.getBirthdate} label="Date de naissance" mode="outlined" />
+                            <MomotoculteurTextInput getData={this.getBirthplace} label="Lieu de naissance" mode="outlined" />
+                            <MomotoculteurTextInput getData={this.getAdress} label="Adresse" mode="outlined" />
+                            <MomotoculteurTextInput getData={this.getCity} label="Ville" mode="outlined" />
+                            <MomotoculteurTextInput getData={this.getPostalcode} label="Code postal" mode="outlined" />
+
+                        </View>
 
                     </ScrollView>
                 </KeyboardAwareScrollView  >
@@ -25,7 +84,9 @@ export default function CreateProfilePage({ navigation }) {
             <View style={styles.viewButtonSection}>
                 <TouchableOpacity
                     style={styles.buttonStyle}
-                    onPress={() => navigation.navigate('createProfil')}
+                    onPress={() => {
+                        this.props.navigation.goBack();
+                    }}
                     activeOpacity={0.7}>
                     <Text style={styles.textStyle}>Valider</Text>
 
@@ -34,4 +95,5 @@ export default function CreateProfilePage({ navigation }) {
         </View>
 
     );
+}
 }
