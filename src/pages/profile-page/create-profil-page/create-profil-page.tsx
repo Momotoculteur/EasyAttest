8fc8f54e4ba0cdfa744aec21d575d704e353f1a6
Bastien MAURICE <bastien_maurice@hotmail.com>
2020-11-05 02:21:35 +0100

    Fix scrollview aware

--- src/pages/profile-page/create-profil-page/create-profil-page.tsx
+++ src/pages/profile-page/create-profil-page/create-profil-page.tsx
@@ -1,33 +1,35 @@
-import { View, Text, TextInput, Button, TouchableOpacity } from "react-native";
+import { View, Text, Button, TouchableOpacity, KeyboardAvoidingView } from "react-native";
 import { styles } from './style'
 import * as React from 'react'
+import { TextInput } from 'react-native-paper';
 import MomotoculteurTextInput from "../../../components/atoms/momotoculteur-text-input/momotoculteurTextInput";
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView, TouchableWithoutFeedback } from "react-native-gesture-handler";
+import MomotoculteurCreateProfilForm from "../../../components/molecules/momotoculteur-create-profil-form/momotoculteurCreateProfilForm";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 export default function CreateProfilePage({ navigation }) {
-
+    const [text, setText] = React.useState('');
     return (
         <View style={styles.global}>
             <View style={styles.viewFormGlobal}>
-                <ScrollView
-                    style={{ flex: 1 }}
-                    keyboardShouldPersistTaps="handled">
-                    <MomotoculteurTextInput placeholder="Prénom" />
-                    <MomotoculteurTextInput placeholder="Nom" />
-                    <MomotoculteurTextInput placeholder="Date de naissance" />
-                    <MomotoculteurTextInput placeholder="Adresse" />
-                    <MomotoculteurTextInput placeholder="Ville" />
-                    <MomotoculteurTextInput placeholder="Code postal" />
-                </ScrollView>
+                <KeyboardAwareScrollView style={{flex: 1}}>
+                    <ScrollView
+                        style={{ flexGrow: 1 }}
+                        keyboardShouldPersistTaps="handled">
+                        <MomotoculteurCreateProfilForm />
+
+                    </ScrollView>
+                </KeyboardAwareScrollView  >
+
             </View>
             <View style={styles.viewButtonSection}>
-                <TouchableOpacity 
-                style={styles.buttonStyle}
+                <TouchableOpacity
+                    style={styles.buttonStyle}
                     onPress={() => navigation.navigate('createProfil')}
                     activeOpacity={0.7}>
                     <Text style={styles.textStyle}>Valider</Text>
 
-                    </TouchableOpacity>
+                </TouchableOpacity>
             </View>
         </View>
 
