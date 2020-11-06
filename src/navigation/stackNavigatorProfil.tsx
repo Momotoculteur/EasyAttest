6163baf543ead248d053ecbeb115f70a4a820dd3
Bastien MAURICE <bastien_maurice@hotmail.com>
2020-11-06 04:42:02 +0100

    profil actif template

--- src/navigation/stackNavigatorProfil.tsx
+++ src/navigation/stackNavigatorProfil.tsx
@@ -4,14 +4,15 @@ import * as React from 'react'
 
 import Profile from '../pages/profile-page/create-profil-page-main'
 import CreateProfilePage from '../pages/profile-page/create-profil-page/create-profil-page'
+import { ROUTE_CONSTANT } from "./route";
 
 const Stack = createStackNavigator();
 
 export default function StackNavigatorProfil() {
     return (
         <Stack.Navigator initialRouteName="profil">
-            <Stack.Screen name="Mes profils" component={Profile} />
-            <Stack.Screen name="createProfil" component={CreateProfilePage} />
+            <Stack.Screen name={ROUTE_CONSTANT.ACTIVE_PROFIL} component={Profile} />
+            <Stack.Screen name={ROUTE_CONSTANT.CREATE_PROFIL} component={CreateProfilePage} />
 
         </Stack.Navigator>
     )
--- src/pages/profile-page/create-profil-page-main.tsx
+++ src/pages/profile-page/create-profil-page-main.tsx
@@ -1,13 +1,19 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useCallback } from 'react';
-import { View, Button, Alert, TouchableOpacity, Text } from 'react-native';
+import { View, Button, Alert, TouchableOpacity, Text, Platform } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { IUser } from '../../components/shared/interface/IUser';
 import DatabaseManager from '../../database/DatabaseManager';
 import { styles } from './styles'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { isEqualIcon } from 'react-native-paper/lib/typescript/src/components/Icon';
+import { Ionicons } from '@expo/vector-icons';
+import { ROUTE_CONSTANT } from '../../navigation/route';
+
 
 interface iState {
-    listAllUsers: IUser[]
+    listAllUsers: IUser[],
+    connectedUser?: IUser
 }
 interface IProps {
 
@@ -33,31 +39,134 @@ export default class ProfilePage extends React.Component<IProps, iState> {
 
     componentDidMount() {
         this.updateListUsers();
+        this.initializeConnectedUser();
         //console.log(this.listAllUsers)
+
+
+    }
+
+    async initializeConnectedUser() {
+
+        /*
+        try {
+            const jsonValue = JSON.stringify({
+                firstName: 'result.firstName',
+                    lastName: 'result.lastName',
+                    adress: 'result.adress',
+                    city: 'result.city',
+                    birthdate: 'result.birthdate',
+                    birthplace: 'result.birthdate',
+                    postalCode: 'result.postalCode'
+            })
+            await AsyncStorage.setItem('@connectedUser', jsonValue)
+          } catch (e) {
+            // saving error
+          }*/
+
+        try {
+            const jsonValue = await AsyncStorage.getItem('@connectedUser')
+            //return jsonValue != null ? JSON.parse(jsonValue) : null;
+            if (jsonValue != null) {
+                this.setState({ connectedUser: JSON.parse(jsonValue) as IUser });
+            }
+        } catch (e) {
+            console.log("ERROR: + " + e)
+        }
     }
 
 
 
     updateListUsers() {
-        DatabaseManager.getAllUser().then((result) => this.setState({listAllUsers: result}) );
+        DatabaseManager.getAllUser().then((result) => this.setState({ listAllUsers: result }));
     }
 
+    /*
+    LISTER TOUT LES USERS
+     <ScrollView style={{flex: 1}}>
+        {this.state.listAllUsers.map((item, index) => {
+            return(<Text>{item.firstName}</Text>)
+        })
+        }
+    </ScrollView>
+    */
+
     render() {
+        //                    {this.state.connectedUser ? <Text>CONNECTED : {this.state.connectedUser.firstName}</Text> : <Text>Aucun</Text>}
 
         return (
             <View style={styles.global}>
+
                 <View style={styles.viewProfilSection}>
-                    <ScrollView style={{flex: 1}}>
-                       {this.state.listAllUsers.map((item, index) => {
-                           return(<Text>{item.firstName}</Text>)
-                       })
-                       }
-                    </ScrollView>
+
+                    <View style={{ flex: 3, flexDirection: 'column', margin: 10, justifyContent: 'space-evenly' }}>
+
+                        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
+                            <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: 40 }}>
+                                <Ionicons name={Platform.OS === 'ios' ? "ios-person" : 'md-person'} size={30} color='#e50d54' />
+                            </View>
+                            <Text>
+                                {this.state.connectedUser?.firstName}{" "}{this.state.connectedUser?.lastName.toUpperCase()}
+                            </Text>
+                        </View>
+
+                        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
+                            <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: 40 }}>
+
+                                <Ionicons name={Platform.OS === 'ios' ? "ios-home" : 'md-home'} size={30} color='#e50d54' />
+                            </View>
+                            <View>
+                                <Text>
+                                    {this.state.connectedUser?.adress}
+                                </Text>
+                                <Text>
+                                    {this.state.connectedUser?.postalCode}{" "}{this.state.connectedUser?.city.toUpperCase()}
+                                </Text>
+                            </View>
+                        </View>
+
+                        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
+                            <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: 40 }}>
+
+                                <Ionicons name={Platform.OS === 'ios' ? "ios-calendar" : 'md-calendar'} size={30} color='#e50d54' />
+                            </View>
+                            <Text>
+                                {this.state.connectedUser?.birthdate}
+                            </Text>
+                        </View>
+
+                        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
+                            <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: 40 }}>
+
+                                <Ionicons name={Platform.OS === 'ios' ? "ios-pin" : 'md-pin'} size={30} color='#e50d54' />
+                            </View>
+                            <Text>
+                                {this.state.connectedUser?.birthplace.toUpperCase()}
+                            </Text>
+                        </View>
+
+
+                    </View>
+                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+                        <View style={{ width: 40, height: 40 }}>
+                            <TouchableOpacity style={{ flex: 1, borderRadius: 20 }}
+                                onPress={() => Alert.alert('lol')}>
+                                <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                                    <Ionicons name={Platform.OS === 'ios' ? "ios-settings" : 'md-settings'} size={30} color='gray' />
+
+                                </View>
+
+                            </TouchableOpacity>
+                        </View>
+
+
+                    </View>
+
                 </View>
+
 
                 <View style={styles.viewButtonSection}>
                     <TouchableOpacity
-                        onPress={() => this.props.navigation.navigate('createProfil')}
+                        onPress={() => this.props.navigation.navigate(ROUTE_CONSTANT.CREATE_PROFIL)}
                         style={styles.buttonStyle}
                         activeOpacity={0.7}>
                         <Text style={styles.textStyle}>Créer profil</Text>
